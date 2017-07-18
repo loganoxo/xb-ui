ce84cd2328460f4ad3e553ecfd0b222433f4005c
ycb <yangzhengming@zhibi365.com>
2017-07-18 20:08:03 +0800

    VueLazyload配置初始化

--- src/main.js
+++ src/main.js
@@ -7,10 +7,20 @@ import VueLazyload from 'vue-lazyload'
 import 'iview/dist/styles/iview.css'
 Vue.config.productionTip = false
 Vue.use(VueLazyload)
+
+/*VueLazyload配置初始化*/
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1
+})
+
 /*注册全局过滤器*/
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
-});
+})
+
 /*根据路由改变页面标题*/
 router.afterEach(to =>{document.title = to.meta.title})
 
