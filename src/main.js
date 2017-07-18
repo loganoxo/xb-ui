f54cfdd1cea5e069ea18de5249126aa734e98bfd
ycb <yangzhengming@zhibi365.com>
2017-07-18 19:32:13 +0800

    注入vuex到根组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from './store/'
+import store from './store'
 import * as filters from './filter/custom'
 import VueLazyload from 'vue-lazyload'
 import 'iview/dist/styles/iview.css'
