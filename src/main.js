2811442a62d1e986ce71479a96cd5f53a915370b
ycb <yangzhengming@zhibi365.com>
2017-07-19 12:24:35 +0800

    引入富文本编辑器 quill-editor  html入口页面加入meta标签

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import router from './router'
 import store from './store'
 import * as filters from './filter/custom'
 import VueLazyload from 'vue-lazyload'
+import VueQuillEditor from 'vue-quill-editor'
 import 'iview/dist/styles/iview.css'
 Vue.config.productionTip = false
 Vue.use(VueLazyload)
+Vue.use(VueQuillEditor)
 
 /*VueLazyload配置初始化*/
 Vue.use(VueLazyload, {
@@ -22,7 +24,9 @@ Object.keys(filters).forEach(key => {
 })
 
 /*根据路由改变页面标题*/
-router.afterEach(to =>{document.title = to.meta.title})
+router.afterEach(to => {
+  document.title = to.meta.title
+})
 
 new Vue({
   el: '#app',
