a21786b94e93064ec7ab9b5e959306f490f63494
ruanhang <ruanhang@zhibitech.com>
2017-08-01 15:57:13 +0800

    端口支持参数化

--- server/config/node.prod.env.js
+++ server/config/node.prod.env.js
@@ -1,8 +1,9 @@
 /**
  * Created by ycb on 2017/7/22.
  */
+var port = normalizePort(process.env.PORT || '3000');
 module.exports = {
-  port: 8080,
+  port: port,
   baseUrl: 'http://192.168.1.142:8765',
   redis: {
     port: '6543',
@@ -10,3 +11,25 @@ module.exports = {
     db: '15'
   }
 };
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+
