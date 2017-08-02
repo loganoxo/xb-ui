baf01b509534f6e36c0c81df7ad8ea1ca7f64ef2
lantaole <609769092@qq.com>
2017-08-02 17:58:21 +0800

    修改生成环境接口地址

--- server/config/node.prod.env.js
+++ server/config/node.prod.env.js
@@ -4,7 +4,7 @@
 const port = normalizePort(process.env.PORT || '3000');
 module.exports = {
   port: port,
-  baseUrl: 'http://192.168.1.142:3000',
+  baseUrl: 'http://192.168.1.142:8765',
   redis: {
     port: '6543',
     host: '192.168.1.44',
@@ -17,7 +17,7 @@ module.exports = {
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  let port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
