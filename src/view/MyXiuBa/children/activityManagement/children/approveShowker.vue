54a0dc15a2c104c86c8dab11a93f6f833e417ec1
lantaole <609769092@qq.com>
2017-09-16 16:33:52 +0800

    通过旺旺获取相关信息接口

--- src/view/MyXiuBa/children/activityManagement/children/approveShowker.vue
+++ src/view/MyXiuBa/children/activityManagement/children/approveShowker.vue
@@ -183,7 +183,11 @@
               </thead>
               <tbody v-if="approveTableList.length > 0" v-for="item in approveTableList" :key="item.id">
               <tr>
-                <td>{{item.alitmAccount}}</td>
+                <td>
+                  <p>{{item.alitmAccount}}</p>
+                  <p><img :src="item.creditLevel" alt=""></p>
+                  <p>淘气值：{{item.tqz}}</p>
+                </td>
                 <td>{{item.orderNum || '------'}}</td>
                 <td>{{getTaskStatus(item.status)}}</td>
                 <td>{{item.updateTime | dateFormat('YYYY-MM-DD hh:mm:ss')}}</td>
@@ -285,6 +289,117 @@
     },
     data() {
       return {
+        taobaoLevelImgs: [
+          {
+            value: 1,
+            text: 'https://img.alicdn.com/newrank/b_red_1.gif',
+            label: '1心'
+          },
+          {
+            value: 2,
+            text: 'https://img.alicdn.com/newrank/b_red_2.gif',
+            label: '2心'
+          },
+          {
+            value: 3,
+            text: 'https://img.alicdn.com/newrank/b_red_3.gif',
+            label: '3心'
+          },
+          {
+            value: 4,
+            text: 'https://img.alicdn.com/newrank/b_red_4.gif',
+            label: '4心'
+          },
+          {
+            value: 5,
+            text: 'https://img.alicdn.com/newrank/b_red_5.gif',
+            label: '5心'
+          },
+          {
+            value: 6,
+            text: 'https://img.alicdn.com/newrank/b_blue_1.gif',
+            label: '1钻'
+          },
+          {
+            value: 7,
+            text: 'https://img.alicdn.com/newrank/b_blue_2.gif',
+            label: '2钻'
+          },
+          {
+            value: 8,
+            text: 'https://img.alicdn.com/newrank/b_blue_3.gif',
+            label: '3钻'
+          },
+          {
+            value: 9,
+            text: 'https://img.alicdn.com/newrank/b_blue_4.gif',
+            label: '4钻'
+          },
+          {
+            value: 10,
+            text: 'https://img.alicdn.com/newrank/b_blue_5.gif',
+            label: '5钻'
+          },
+          {
+            value: 11,
+            text: 'https://img.alicdn.com/newrank/s_crown_1.gif',
+            label: '1皇冠'
+          },
+          {
+            value: 12,
+            text: 'https://img.alicdn.com/newrank/s_crown_2.gif',
+            label: '2皇冠'
+          },
+          {
+            value: 13,
+            text: 'https://img.alicdn.com/newrank/s_crown_3.gif',
+            label: '3皇冠'
+          },
+          {
+            value: 14,
+            text: 'https://img.alicdn.com/newrank/s_crown_4.gif',
+            label: '4皇冠'
+          },
+          {
+            value: 15,
+            text: 'https://img.alicdn.com/newrank/s_crown_5.gif',
+            label: '5皇冠'
+          },
+        ],
+        taoqizhiList: [
+          {
+            value: 1,
+            label: '0-199'
+          },
+          {
+            value: 2,
+            label: '200-399'
+          },
+          {
+            value: 3,
+            label: '400-599'
+          },
+          {
+            value: 4,
+            label: '600-799'
+          },
+          {
+            value: 5,
+            label: '800-999'
+          },
+          {
+            value: 6,
+            label: '1000-1999'
+          },
+          {
+            value: 7,
+            label: '2000-2499'
+          },
+          {
+            value: 8,
+            label: '2500以上'
+          },
+        ],
         showApproveStatus: 'toAudit',
         taskId: null,
         checkAllByPass: false,
@@ -406,7 +521,10 @@
               api.getAlitmByAccount({
                 account: _this.approveTableList[i].alitmAccount,
               }).then((res) => {
-                  console.log(res);
+                if(res.status){
+                  _this.approveTableList[i].creditLevel = _this.taobaoLevelImgs[parseInt(res.data.creditLevel) -1].text;
+                  _this.approveTableList[i].tqz = _this.taoqizhiList[parseInt(res.data.tqz) - 1].label;
+                }
               })
             }
             _this.approveTaskInfo = res.data.taskInfo;
