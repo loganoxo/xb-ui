2beb56344aac8dcb0c4ee4d227531280ad5ca7c2
ycb <yangzhengming@zhibi365.com>
2017-10-17 10:51:30 +0800

    增加路由匹配错误处理  增加百度统计代码

---
+++ static/js/baidu-statistics.js
@@ -0,0 +1,13 @@
+var _hmt = _hmt || [];
+
+(function() {
+
+  var hm = document.createElement("script");
+
+  hm.src = "https://hm.baidu.com/hm.js?daf012169d18e9a97d2d5eac5dce0b73";
+
+  var s = document.getElementsByTagName("script")[0];
+
+  s.parentNode.insertBefore(hm, s);
+
+})();
