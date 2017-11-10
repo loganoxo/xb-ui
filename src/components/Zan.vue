a53c8a88fccb8a27fc9c13b1ce86712dd0751c87
ycb <yangzhengming@zhibi365.com>
2017-11-10 14:40:00 +0800

    代码优化 发布页折扣样式修改

---
+++ src/components/Zan.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="VueStar">
+    <div class="VueStar-ground">
+      <div class="VueStar-icon" @click="toggle" :class="AnimateClass" :style='{color:ColorValue}'>
+        <Icon type="android-favorite"></Icon>
+      </div>
+      <div class="VueStar-decoration" :class="{ 'active':active}"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Icon from 'iview/src/components/icon'
+  export default {
+    name:'zan',
+    components: {
+      Icon: Icon
+    },
+    props: {
+      animate: {
+        type:String,
+        default:'animated zoomInDown'
+      },
+      color:{
+        type:String,
+        default:'DD4D4E'
+      }
+    },
+    data() {
+      return {
+        active: false,
+        toggleAnimate: false,
+        toggleColor: false
+      }
+    },
+    mounted() {
+
+    },
+    created() {
+
+    },
+    computed: {
+      AnimateClass () {
+        return this.toggleAnimate ? this.animate : ''
+      },
+      ColorValue () {
+        return this.toggleColor ? this.color : ''
+      }
+    },
+    methods: {
+      toggle () {
+        this.active = !this.active;
+        this.toggleAnimate = !this.toggleAnimate;
+        this.toggleColor = !this.toggleColor
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .VueStar{
+    position: absolute;
+  }
+  .VueStar-ground {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .VueStar-icon {
+    z-index: 888;
+  }
+  .VueStar-decoration {
+    width: 100px;
+    height: 100px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: url('~assert/img/common/zan-image.png') no-repeat;
+    background-position: 0 0;
+    transition: background-position 1s steps(25);
+    transition-duration: 0s;
+    .active {
+      transition-duration: 1s;
+      background-position: -2500px 0;
+    }
+  }
+</style>
--- src/view/TrialReport.vue
+++ src/view/TrialReport.vue
@@ -50,8 +50,7 @@
                   :total= totalPages
                   :page-size = trialReportParams.pageSize
                   @on-change = pageChange
-                  show-elevator
-                ></Page>
+                  show-elevator></Page>
               </div>
             </div>
           </div>
@@ -65,9 +64,9 @@
             </div>
             <div class="trial-account-details">
               <div class="task-info">
-                <img :src="showkerReportDesc.task.taskMainImage + '!orgi75'" alt="" width="100px" class="left">
+                <router-link tag="img" :to="{path: '/task-details',query:{q: encryptionId(showkerReportDesc.task.id)}}" :src="showkerReportDesc.task.taskMainImage + '!orgi75'" alt="" width="100" class="left cursor-p"></router-link>
                 <div class="left ml-20">
-                  <p>{{showkerReportDesc.task.taskName}}</p>
+                  <router-link :to="{path: '/task-details',query:{q: encryptionId(showkerReportDesc.task.id)}}">{{showkerReportDesc.task.taskName}}</router-link>
                   <p>宝贝单价&nbsp;<span>{{showkerReportDesc.task.itemPrice / 100}}</span>&nbsp;元 。</p>
                 </div>
               </div>
@@ -202,6 +201,9 @@
       }
     },
     methods: {
+      encryptionId(id) {
+        return encryption(id);
+      },
       getTagTrialReports(key){
         this.trialReportParams.itemCatalogname = key;
         this.getTrialReports();
@@ -341,7 +343,7 @@
       width: 100%;
       margin-top: 5px;
       .task-info{
-        p{
+        a,p{
           line-height: 30px;
           font-size: 14px;
         }
