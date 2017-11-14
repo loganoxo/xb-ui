88b763f3f18e67861c90ba311325392d101f7e07
wangwentao <wangwentao@zhibitech.com>
2017-11-14 16:13:51 +0800

    买家秀列表展示旺旺号信息

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -189,7 +189,7 @@
               </div>
               <ul class="clear" :class="[leftSlider ? 'slider-top-active-left' : 'slider-top-default-left']"
                   @mouseover="clearLeftSliderFunc()" @mouseleave="leftSliderFunc()">
-                <li v-for="item in buyerShowList" class="content cursor-p left pos-rel" v-show="item.trialReportImages">
+                <li v-for="item in buyerShowList" class="content cursor-p left pos-rel" >
                   <router-link :to="{path:'/trial-report',query:{q:encryptionId(item.showkerId),showReportDesc:true,id:encryptionId(item.id)}}" :title="item.task.taskName">
                     <div style="height: 260px">
                       <img :src="item.trialReportImages+'!thum200'" alt="" width="200" height="260">
@@ -208,15 +208,13 @@
                     <a class="des-text" :title="item.trialReportText">{{item.trialReportText}}</a>
                   </p>
                   <div class="clear bottom mt-20">
-                    <img class="left" width="48" height="48" src="~assets/img/common/avatar/tx1.png" alt="">
+                    <router-link :to="{path:'/trial-report',query:{q:encryptionId(item.showkerId)}}"><img class="left" width="48" height="48" :src="item.showkerPortraitPic" alt=""></router-link>
                     <div class="left ml-10" style="margin-top: 5px">
                       <p style="color: #000">{{item.showkerPhone}}</p>
-                      <!--<p class="click-good">
-                        <Icon class="icon-heart" type="heart"></Icon>
-                        <span>赞（12000）</span>
-                      </p>-->
+                      <img :src="taobaoLevelImgs[item.other.creditLevel?parseInt(item.other.creditLevel)-2:2].text" alt="">
                     </div>
                   </div>
+                  <div class="text-ct"><span>淘气值：{{taoqizhiList[item.other.tqz?parseInt(item.other.tqz)-1:3].label}}</span></div>
                 </li>
               </ul>
             </div>
@@ -601,6 +599,112 @@
         },
         buyerShowList:[],
         getMoreBuyerShow:false,
+        taobaoLevelImgs: [
+          {
+            value: 2,
+            text: 'https://img.alicdn.com/newrank/b_red_2.gif',
+            label: '2心'
+          },
+          {
+            value: 3,
+            text: 'https://img.alicdn.com/newrank/b_red_3.gif',
+            label: '3心'
+          },
+          {
+            value: 4,
+            text: 'https://img.alicdn.com/newrank/b_red_4.gif',
+            label: '4心'
+          },
+          {
+            value: 5,
+            text: 'https://img.alicdn.com/newrank/b_red_5.gif',
+            label: '5心'
+          },
+          {
+            value: 6,
+            text: 'https://img.alicdn.com/newrank/b_blue_1.gif',
+            label: '1钻'
+          },
+          {
+            value: 7,
+            text: 'https://img.alicdn.com/newrank/b_blue_2.gif',
+            label: '2钻'
+          },
+          {
+            value: 8,
+            text: 'https://img.alicdn.com/newrank/b_blue_3.gif',
+            label: '3钻'
+          },
+          {
+            value: 9,
+            text: 'https://img.alicdn.com/newrank/b_blue_4.gif',
+            label: '4钻'
+          },
+          {
+            value: 10,
+            text: 'https://img.alicdn.com/newrank/b_blue_5.gif',
+            label: '5钻'
+          },
+          {
+            value: 11,
+            text: 'https://img.alicdn.com/newrank/s_crown_1.gif',
+            label: '1皇冠'
+          },
+          {
+            value: 12,
+            text: 'https://img.alicdn.com/newrank/s_crown_2.gif',
+            label: '2皇冠'
+          },
+          {
+            value: 13,
+            text: 'https://img.alicdn.com/newrank/s_crown_3.gif',
+            label: '3皇冠'
+          },
+          {
+            value: 14,
+            text: 'https://img.alicdn.com/newrank/s_crown_4.gif',
+            label: '4皇冠'
+          },
+          {
+            value: 15,
+            text: 'https://img.alicdn.com/newrank/s_crown_5.gif',
+            label: '5皇冠'
+          },
+        ],
+        taoqizhiList: [
+          {
+            value: 1,
+            label: '0-199'
+          },
+          {
+            value: 2,
+            label: '200-399'
+          },
+          {
+            value: 3,
+            label: '400-599'
+          },
+          {
+            value: 4,
+            label: '600-799'
+          },
+          {
+            value: 5,
+            label: '800-999'
+          },
+          {
+            value: 6,
+            label: '1000-1999'
+          },
+          {
+            value: 7,
+            label: '2000-2499'
+          },
+          {
+            value: 8,
+            label: '2500以上'
+          },
+        ],
       }
     },
     created() {
