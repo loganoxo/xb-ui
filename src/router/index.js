d3f02b122b363949652276e240ec4adef117f1bf
ycb <yangzhengming@zhibi365.com>
2017-11-29 17:49:53 +0800

    修改旺旺绑定页面权限为秀客

--- src/router/index.js
+++ src/router/index.js
@@ -547,7 +547,7 @@ export default new Router({
                 logInAuthority: true,
                 topShow: true,
                 bottomShow: true,
-                role: null,
+                role: 0,
               },
             },
             {
