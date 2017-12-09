ead246cdcc8215ea5a6e03a4bf43adef0cea4b08
ycb <yangzhengming@zhibi365.com>
2017-12-09 17:58:47 +0800

    修复使用QQ登录时登录成功之后会跳转到登录页

--- src/view/QQLogin.vue
+++ src/view/QQLogin.vue
@@ -11,60 +11,58 @@
 <script>
   import Spin from 'iview/src/components/spin'
   import api from '@/config/apiConfig'
-  import RoleTop from '@/components/RoleTop.vue'
-  import {setStorage, getStorage, removeStorage} from '@/config/utils'
+  import {setStorage} from '@/config/utils'
+
   export default {
     name: 'QQLogin',
     components: {
       Spin: Spin,
-      RoleTop: RoleTop,
     },
     data() {
       return {
         accessToken: {},
-        imgSrc: null,
-        modal1: true,
       }
     },
-    mounted() {
-    },
+    mounted() {},
     computed: {},
     created() {
-       if(this.$route.hash){
-         let hash = this.$route.hash.replace('#','');
-         hash = hash.split("&");
-         let array = [];
-         for(let i = 0, j = hash.length; i < j; i++){
-            array = hash[i].split("=");
-            if(array[0] === 'access_token'){
-              this.accessToken.accessToken = array[1];
-            }
-         }
-         this.setUserInfo()
-       }
+      let _this = this;
+      if (_this.$route.hash) {
+        let hash = _this.$route.hash.replace('#', '');
+        hash = hash.split("&");
+        let array = [];
+        for (let i = 0, j = hash.length; i < j; i++) {
+          array = hash[i].split("=");
+          if (array[0] === 'access_token') {
+            _this.accessToken.accessToken = array[1];
+            break;
+          }
+        }
+        _this.setUserInfo();
+      }
     },
     methods: {
       setUserInfo() {
-        let self = this;
+        let _this = this;
         api.QQLoginFunc({
-          accessToken: self.accessToken.accessToken
-        }).then((res) => {
+          accessToken: _this.accessToken.accessToken
+        }).then(res => {
           if (res.status) {
-            self.$store.commit({
+            _this.$store.commit({
               type: 'RECORD_USER_INFO',
               info: res.data
             });
-//            self.$store.dispatch('getDetectionMerchantGuide');
+//            _this.$store.dispatch('getDetectionMerchantGuide');
             setStorage('weChartPop', 1);
-            self.$router.go(-2);
+            _this.$router.push('Home');
           } else {
-            if(res.statusCode === 'new_user'){
-              self.$router.push({path: '/sel-role',query: {accessToken: self.accessToken.accessToken, qqOpenId: res.msg}});
-            }else {
-              self.$Message.error({
-                content: res.msg,
-                duration: 6
+            if (res.statusCode === 'new_user') {
+              _this.$router.push({
+                path: '/sel-role',
+                query: {accessToken: _this.accessToken.accessToken, qqOpenId: res.msg}
               });
+            } else {
+              _this.$Message.error(res.msg);
             }
           }
         })
@@ -74,14 +72,15 @@
 </script>
 
 <style lang="scss" scoped>
-.qq-login{
-  margin: 200px auto;
-  text-align: center;
-}
-.spin{
-  width: 20px;
-  margin: auto
-}
+  .qq-login {
+    margin: 200px auto;
+    text-align: center;
+  }
+
+  .spin {
+    width: 20px;
+    margin: auto
+  }
 </style>
 
 
