ad457970163c81cd306ee8a6253466207be0a3ab
wangwentao <wangwentao@zhibitech.com>
2018-04-20 11:56:42 +0800

    商家充值会员Bug修复

--- src/view/MyXiuBa/children/VipMember.vue
+++ src/view/MyXiuBa/children/VipMember.vue
@@ -126,7 +126,7 @@
     data() {
       return {
         isSelect: 'one',
-        day: 730,
+        day: 365,
         year: '0年',
         rechargeSum: '0',
         needRecharge: 0,
@@ -173,14 +173,23 @@
       changeStyle(select, day, year, recharge, level, id) {
         let _this = this;
         let stopTime = null;
+        console.log(level);
+        console.log(recharge);
         _this.isSelect = select;
         _this.year = year;
         _this.memberLevel = level;
         _this.memberId = id;
+        _this.rechargeSum = 0;
+        console.log(_this.rechargeSum);
         if (level === _this.getMemberLevel) {
           stopTime = new Date().getTime() + parseInt(day) * 24 * 3600 * 1000 + _this.timeRemaining * 24 * 3600 * 1000;
           _this.endTime = stopTime;
           _this.rechargeSum =(recharge / 100).toFixed(2);
+          console.log('-------------');
+          console.log(111);
+          console.log(recharge);
+          console.log(_this.rechargeSum);
+          console.log('------------');
           if (_this.rechargeSum - _this.getUserBalance < 0) {
             _this.needRecharge = 0
           } else {
@@ -189,7 +198,12 @@
         } else {
           stopTime = new Date().getTime() + parseInt(day) * 24 * 3600 * 1000;
           _this.endTime = stopTime;
+          console.log(recharge);
+          console.log(_this.moneyRemaining);
           _this.rechargeSum = (recharge / 100 - _this.moneyRemaining/100).toFixed(2);
+          console.log(222);
+          console.log(_this.rechargeSum);
+          console.log(333);
           if (_this.rechargeSum - _this.getUserBalance < 0) {
             _this.needRecharge = 0
           } else {
@@ -215,7 +229,7 @@
       },
       getUserMemberLevelInfo() {
         let _this = this;
-        if (!_this.getMemberLevel) {
+        if (!_this.getMemberLevel || _this.membershipIsExpire) {
           _this.getUserMemberAll();
           return;
         }
