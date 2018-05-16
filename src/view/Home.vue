051c385536bd24bead3399fc9d3e9cfcb5836def
ycb <yangzhengming@zhibi365.com>
2018-05-16 16:18:03 +0800

    主页会员显示bug修复

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -105,12 +105,12 @@
                     Hi~ 商家 {{decodeURIComponent(getUserInfoPhone)}}
                   </router-link>
                   <div v-if="getUserInfoRole === 1 && !isMember" class="fs-12">
-                    <Icon type="social-vimeo" class="cl999"></Icon>
                     <span>非会员</span>
-                    <router-link to="/user/vip-member">马上开通会员</router-link>
+                    <router-link to="/user/vip-member/order">马上开通会员</router-link>
                   </div>
                   <div v-if="getUserInfoRole === 1 && isMember" class="fs-12">
-                    <Icon type="social-vimeo" class="cl-red"></Icon>
+                    <img v-if="getMemberVersionLevel === 200" src="~assets/img/common/vip.png" alt="vipLogo">
+                    <img v-if="getMemberVersionLevel === 300" src="~assets/img/common/svip.png" alt="svipLogo">
                     <span class="cl-red">您已是VIP，发布活动免费无上限</span>
                   </div>
                 </div>
@@ -714,6 +714,9 @@
       },
       getMemberDeadline() {
         return this.$store.state.userInfo.memberDeadline
+      },
+      getMemberVersionLevel() {
+        return this.$store.state.userInfo.memberLevel
       },
       isMember() {
         return this.$store.getters.isMemberOk
