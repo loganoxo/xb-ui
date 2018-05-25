c7a1b125142c8a7aca31bfa0b0cc3f6e5d3e8264
ycb <yangzhengming@zhibi365.com>
2018-05-25 20:02:25 +0800

    火眼金睛

--- src/view/personalCenter/children/moneyManagement/children/transactionRecord.vue
+++ src/view/personalCenter/children/moneyManagement/children/transactionRecord.vue
@@ -57,7 +57,7 @@
           </td>
           <td>
             <p>{{getTradType(tbodyDetails.accountChangeType)}}</p>
-            <p v-if="tbodyDetails.accountChangeType !== 3 && tbodyDetails.accountChangeType !== 1 && tbodyDetails.accountChangeType !== 2">活动编号：{{tbodyDetails.taskSerialNum}}</p>
+            <p v-if="tbodyDetails.accountChangeType !== 1 && tbodyDetails.accountChangeType !== 2 && tbodyDetails.accountChangeType !== 3 && tbodyDetails.accountChangeType !== 10 && tbodyDetails.accountChangeType !== 11">活动编号：{{tbodyDetails.taskSerialNum}}</p>
           </td>
           <td :class="{tdColor:tbodyDetails.amountChange<0 , tdColorGreen:tbodyDetails.amountChange>0}">
             {{typeChang(tbodyDetails.amountChange / 100) || 0}}
