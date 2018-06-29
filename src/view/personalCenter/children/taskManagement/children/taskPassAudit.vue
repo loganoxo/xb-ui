545eaf7957992e05f37971003e25509826e297ae
ycb <yangzhengming@zhibi365.com>
2018-06-29 18:42:17 +0800

    修复任务管理已通过列表中审核成功订单后订单状态数量显示BUG

--- src/view/personalCenter/children/taskManagement/children/taskPassAudit.vue
+++ src/view/personalCenter/children/taskManagement/children/taskPassAudit.vue
@@ -52,125 +52,127 @@
       <i-button size="large" class="mr-10" :loading="batchPassLoading" @click="readyBatchPass"><icon type="android-done-all"/>批量通过所有待审核订单</i-button>
       <i-button size="large" @click="batchExportModel = true"><icon type="ios-download-outline"/> 批量导出以下所有订单号</i-button>
     </div>
-    <div class="mt-12" v-for="(item,index) in taskPassAuditList" :key="item.id" v-if="taskPassAuditList.length > 0">
-      <div class="collapse-header clear" @click.self="collapseToggle(item.id,index)" :class="{noBorderRadius:selectId}">
-        <div class="manage-img inline-block">
-          <img :src="item.taskMainImage | imageSrc('!thum54')" alt="活动主图">
-          <span v-if="item.zone === 'certainly_hit'" class="certainly-hit-tip">推荐必中</span>
-        </div>
-        <div class="manage-text left ml-5 inline-block mt-8">
-          <p>
-            <span>活动编号：{{item.number}}</span>
-            <span class="ml-5">（{{item.taskStatusDesc}} / {{item.settlementStatusDesc}}）</span>
-          </p>
-          <p>活动名称：{{item.taskName}}</p>
-        </div>
-        <div class="waiting-task-number-pass">
-          <p class="task-pass"
-             :class="{'line-height':showkerTaskStatusList.length === 0 || showkerTaskStatusList.length === 8 || showkerTaskStatusList.length > 4}">
+    <template v-if="taskPassAuditList.length > 0">
+      <div class="mt-12" v-for="(item,index) in taskPassAuditList" :key="item.id">
+        <div class="collapse-header clear" @click.self="collapseToggle(item.id,index)" :class="{noBorderRadius: selectId}">
+          <div class="manage-img inline-block">
+            <img :src="item.taskMainImage | imageSrc('!thum54')" alt="活动主图">
+            <span v-if="item.zone === 'certainly_hit'" class="certainly-hit-tip">推荐必中</span>
+          </div>
+          <div class="manage-text left ml-5 inline-block mt-8">
+            <p>
+              <span>活动编号：{{item.number}}</span>
+              <span class="ml-5">（{{item.taskStatusDesc}} / {{item.settlementStatusDesc}}）</span>
+            </p>
+            <p>活动名称：{{item.taskName}}</p>
+          </div>
+          <div class="waiting-task-number-pass">
+            <p class="task-pass"
+               :class="{'line-height':showkerTaskStatusList.length === 0 || showkerTaskStatusList.length === 8 || showkerTaskStatusList.length > 4}">
             <span v-show="showkerTaskStatusList.length === 0 || showkerTaskStatusList.length === 8">全部<span
               class="main-color">({{item.allTaskNum || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('pass_and_unclaimed',showkerTaskStatusList)"
-                  class="ml-5">已通过待下单<span class="main-color">({{item.pass_and_unclaimed || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('order_num_waiting_audit',showkerTaskStatusList)"
-                  class="ml-5">订单号待审核<span class="main-color">({{item.order_num_waiting_audit || 0}})</span></span>
-            <span
-              v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_waiting_submit',showkerTaskStatusList)"
-              class="ml-5">已下单待交买家秀<span class="main-color">({{item.trial_report_waiting_submit || 0}})</span></span>
-            <span
-              v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_waiting_confirm',showkerTaskStatusList)">买家秀待确认<span
-              class="main-color">({{item.trial_report_waiting_confirm || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_finished',showkerTaskStatusList)"
-                  class="ml-5">活动完成<span class="main-color">({{item.trial_finished || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('order_num_error',showkerTaskStatusList)"
-                  class="ml-5">订单信息有误<span class="main-color">({{item.order_num_error || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_unqualified',showkerTaskStatusList)"
-                  class="ml-5">买家秀不合格<span class="main-color">({{item.trial_report_unqualified || 0}})</span></span>
-            <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_end',showkerTaskStatusList)" class="ml-5">任务终止<span
-              class="main-color">({{item.trial_end || 0}})</span></span>
-          </p>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('pass_and_unclaimed',showkerTaskStatusList)"
+                    class="ml-5">已通过待下单<span class="main-color">({{item.pass_and_unclaimed || 0}})</span></span>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('order_num_waiting_audit',showkerTaskStatusList)"
+                    class="ml-5">订单号待审核<span class="main-color">({{item.order_num_waiting_audit || 0}})</span></span>
+              <span
+                v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_waiting_submit',showkerTaskStatusList)"
+                class="ml-5">已下单待交买家秀<span class="main-color">({{item.trial_report_waiting_submit || 0}})</span></span>
+              <span
+                v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_waiting_confirm',showkerTaskStatusList)">买家秀待确认<span
+                class="main-color">({{item.trial_report_waiting_confirm || 0}})</span></span>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_finished',showkerTaskStatusList)"
+                    class="ml-5">活动完成<span class="main-color">({{item.trial_finished || 0}})</span></span>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('order_num_error',showkerTaskStatusList)"
+                    class="ml-5">订单信息有误<span class="main-color">({{item.order_num_error || 0}})</span></span>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_report_unqualified',showkerTaskStatusList)"
+                    class="ml-5">买家秀不合格<span class="main-color">({{item.trial_report_unqualified || 0}})</span></span>
+              <span v-show="showkerTaskStatusList.length === 0 || oneOf('trial_end',showkerTaskStatusList)" class="ml-5">任务终止<span
+                class="main-color">({{item.trial_end || 0}})</span></span>
+            </p>
+          </div>
+          <icon :class="{'show-table-styles' : selectId === item.id}" class="mt-15" type="arrow-right-b"/>
         </div>
-        <icon :class="{'show-table-styles' : selectId === item.id}" class="mt-15" type="arrow-right-b"/>
-      </div>
-      <collapse-transition>
-        <div class="task-table" v-show="selectId === item.id">
-          <table>
-            <thead>
-            <tr>
-              <th width="25%">淘宝账号（旺旺号）</th>
-              <th width="15%">QQ号码</th>
-              <th width="15%">状态</th>
-              <th width="15%">订单号</th>
-              <th width="10%">实付金额（元）</th>
-              <th width="20%">操作</th>
-            </tr>
-            </thead>
-            <tbody v-for="item in item.passTask" :key="item.applyId">
-            <tr>
-              <td>
-                <p>{{item.alitmAccount}}</p>
-                <p><img :src="item.creditLevel" alt="" style="width: auto;height: auto;"></p>
-                <p v-if="item.tqz">淘气值：{{item.tqz}}</p>
-                <p v-cloak>申请次数：{{item.applyCount || 0}}</p>
-                <p v-cloak>成功次数：{{item.applySuccessCount || 0}}</p>
-              </td>
-              <td>{{item.qqNumber || '------'}}</td>
-              <td>
-                <p v-if="item.status === 'order_num_error'|| item.status === 'trial_report_unqualified'">
-                  <tooltip
-                    :content="item.auditDescription"
-                    placement="top" class="cursor-p">
-                    <icon color="#f9284f" type="information-circled"/>
-                    <span class="main-color">{{getTaskStatus(item.status)}}</span>
-                  </tooltip>
-                </p>
-                <p v-if="item.status !== 'order_num_error' && item.status !== 'trial_end' && item.status !== 'trial_report_unqualified'">
-                  {{getTaskStatus(item.status)}}</p>
-                <p v-if="item.status !== 'trial_end' && item.status !== 'trial_finished'">
-                  <time-down color='#ff4040' :fontWeight=600 :endTime="item.currentGenerationEndTime"/>
-                </p>
-                <p v-if="item.status === 'trial_end'">
-                  <tooltip :content="item.trialEndReason === 'admin_manual_close' ? getTaskStatus(item.trialEndReason) +'：'+ item.auditDescription : getTaskStatus(item.trialEndReason)"
-                    placement="top" class="cursor-p">
-                    <icon color="#f9284f" type="information-circled"/>
-                    <span class="main-color">{{getTaskStatus(item.status)}}</span>
-                  </tooltip>
-                </p>
-              </td>
-              <td>{{item.orderNum || '------'}}</td>
-              <td>{{(item.orderPrice).toFixed(2)}}</td>
-              <td>
-                <p class="del-edit">
+        <collapse-transition>
+          <div class="task-table" v-show="selectId === item.id">
+            <table>
+              <thead>
+              <tr>
+                <th width="25%">淘宝账号（旺旺号）</th>
+                <th width="15%">QQ号码</th>
+                <th width="15%">状态</th>
+                <th width="15%">订单号</th>
+                <th width="10%">实付金额（元）</th>
+                <th width="20%">操作</th>
+              </tr>
+              </thead>
+              <tbody v-for="item in item.passTask" :key="item.applyId">
+              <tr>
+                <td>
+                  <p>{{item.alitmAccount}}</p>
+                  <p><img :src="item.creditLevel" alt="" style="width: auto;height: auto;"></p>
+                  <p v-if="item.tqz">淘气值：{{item.tqz}}</p>
+                  <p v-cloak>申请次数：{{item.applyCount || 0}}</p>
+                  <p v-cloak>成功次数：{{item.applySuccessCount || 0}}</p>
+                </td>
+                <td>{{item.qqNumber || '------'}}</td>
+                <td>
+                  <p v-if="item.status === 'order_num_error'|| item.status === 'trial_report_unqualified'">
+                    <tooltip
+                      :content="item.auditDescription"
+                      placement="top" class="cursor-p">
+                      <icon color="#f9284f" type="information-circled"/>
+                      <span class="main-color">{{getTaskStatus(item.status)}}</span>
+                    </tooltip>
+                  </p>
+                  <p v-if="item.status !== 'order_num_error' && item.status !== 'trial_end' && item.status !== 'trial_report_unqualified'">
+                    {{getTaskStatus(item.status)}}</p>
+                  <p v-if="item.status !== 'trial_end' && item.status !== 'trial_finished'">
+                    <time-down color='#ff4040' :fontWeight=600 :endTime="item.currentGenerationEndTime"/>
+                  </p>
+                  <p v-if="item.status === 'trial_end'">
+                    <tooltip :content="item.trialEndReason === 'admin_manual_close' ? getTaskStatus(item.trialEndReason) +'：'+ item.auditDescription : getTaskStatus(item.trialEndReason)"
+                             placement="top" class="cursor-p">
+                      <icon color="#f9284f" type="information-circled"/>
+                      <span class="main-color">{{getTaskStatus(item.status)}}</span>
+                    </tooltip>
+                  </p>
+                </td>
+                <td>{{item.orderNum || '------'}}</td>
+                <td>{{(item.orderPrice).toFixed(2)}}</td>
+                <td>
+                  <p class="del-edit">
                   <span v-if="item.status === 'order_num_waiting_audit'"
                         @click="openCheckOrder(item.id, item.needBrowseCollectAddCart, item.itemIssue, index)">审核订单信息</span>
-                  <span v-if="item.status === 'trial_report_waiting_confirm'"
-                        @click="goProbationReport(item.id)">审核买家秀</span>
-                  <!--<router-link target="_blank" :to="{path:'/user/activity-management/report',query:{id: encryptionId(item.id), from: 'taskPassAudit'}}" v-if="item.status === 'trial_report_waiting_confirm'">-->
+                    <span v-if="item.status === 'trial_report_waiting_confirm'"
+                          @click="goProbationReport(item.id)">审核买家秀</span>
+                    <!--<router-link target="_blank" :to="{path:'/user/activity-management/report',query:{id: encryptionId(item.id), from: 'taskPassAudit'}}" v-if="item.status === 'trial_report_waiting_confirm'">-->
                     <!--审核买家秀-->
-                  <!--</router-link>-->
-                  <span v-if="item.status === 'trial_finished' && !item.ifEvaluated"
-                        @click="getShowkerReportInfo(item.id,item.alitmAccount)">评价拿手</span>
-                  <span v-if="item.status !== 'order_num_waiting_audit' && item.status !== 'trial_report_waiting_confirm' && !(item.status === 'trial_finished' && !item.ifEvaluated)">------</span>
-                </p>
-              </td>
-            </tr>
-            </tbody>
-            <thead v-if="taskPassAuditList[index].passTask && taskPassAuditList[index].passTask.length > 0">
-            <tr>
-              <td colspan="6">
-                <page :total="taskTotalElements" :page-size="taskPageSize" :current="taskPageIndex" @on-change="TaskPageChange"/>
-              </td>
-            </tr>
-            </thead>
-            <thead v-if="taskPassAuditList[index].passTask && taskPassAuditList[index].passTask.length === 0">
-            <tr>
-              <td colspan="6" width="100%">暂无数据</td>
-            </tr>
-            </thead>
-          </table>
-        </div>
-      </collapse-transition>
-    </div>
+                    <!--</router-link>-->
+                    <span v-if="item.status === 'trial_finished' && !item.ifEvaluated"
+                          @click="getShowkerReportInfo(item.id,item.alitmAccount)">评价拿手</span>
+                    <span v-if="item.status !== 'order_num_waiting_audit' && item.status !== 'trial_report_waiting_confirm' && !(item.status === 'trial_finished' && !item.ifEvaluated)">------</span>
+                  </p>
+                </td>
+              </tr>
+              </tbody>
+              <thead v-if="taskPassAuditList[index].passTask && taskPassAuditList[index].passTask.length > 0">
+              <tr>
+                <td colspan="6">
+                  <page :total="taskTotalElements" :page-size="taskPageSize" :current="taskPageIndex" @on-change="TaskPageChange"/>
+                </td>
+              </tr>
+              </thead>
+              <thead v-if="taskPassAuditList[index].passTask && taskPassAuditList[index].passTask.length === 0">
+              <tr>
+                <td colspan="6" width="100%">暂无数据</td>
+              </tr>
+              </thead>
+            </table>
+          </div>
+        </collapse-transition>
+      </div>
+    </template>
     <div class="text-ct mt-40">{{dataStatusTip}}</div>
     <div class="activity-page mt-20 right mr-10" v-if="taskPassAuditList && taskPassAuditList.length > 0">
       <page :total="totalElements" :page-size="pageSize" :current="pageIndex" @on-change="pageChange"/>
@@ -554,9 +556,10 @@
         api.orderNumberInfo({id: id}).then(res => {
           if (res.status) {
             _this.orderInfo = res.orderInfo;
-            if(_this.taskPassAuditList[index].order_num_waiting_audit > 0) {
-              _this.taskPassAuditList[index].order_num_waiting_audit -=1;
+            if (_this.taskPassAuditList[index].order_num_waiting_audit > 0) {
+              _this.taskPassAuditList[index].order_num_waiting_audit--;
             }
+            _this.taskPassAuditList[index].trial_report_waiting_submit++;
           } else {
             _this.$Message.error(res.msg)
           }
