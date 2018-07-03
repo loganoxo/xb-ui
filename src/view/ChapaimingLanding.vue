5b9fd41511b1a15f4d6c7e5ae46e5255ec7553ba
liuwei <liuwei@zhibitech.com>
2018-07-03 22:07:42 +0800

    查排名落地页路由

---
+++ src/view/ChapaimingLanding.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="chapaiming-landing">
+      <top/>
+
+    </div>
+</template>
+
+<script>
+  import Top from '@/components/Top.vue'
+  export default {
+    name: "chapaiming-landing",
+    components: {
+      Top:Top
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @import "src/css/mixin";
+</style>
