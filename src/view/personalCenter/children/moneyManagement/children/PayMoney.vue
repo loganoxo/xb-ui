099867bffcac06a73168826cc4e9064fb9529bf9
liuwei <liuwei@zhibitech.com>
2018-08-17 17:06:14 +0800

    人工充值申请对接

--- src/view/personalCenter/children/moneyManagement/children/PayMoney.vue
+++ src/view/personalCenter/children/moneyManagement/children/PayMoney.vue
@@ -6,96 +6,159 @@
                    class="right cursor-p" style="color: blue;">查看充值记录
       </router-link>
     </div>
-    <div class="my-pay-desc">
-      <iForm :model="payMoney" :label-width="200" :rules="payMoneyRule">
-        <Form-item label="请输入充值金额:" prop="number">
-          <iInput v-model="payMoney.number" @on-blur="" style="width: 200px"></iInput>
-          <span class="ml-10">元(最低1元起充)</span>
-        </Form-item>
-        <Form-item label="请选择支付方式:" prop="payMode">
-          <Radio-group v-model.number="payMoney.payMode">
-            <Radio label="ali">
-              <img src="~assets/img/task-release/zfb_logo.png" alt="支付宝" class="vtc-mid">
-            </Radio>
-            <!--<Radio label="weiXin">-->
-            <!--<img src="~assets/img/task-release/wechat_logo.png" class="vtc-mid">-->
-            <!--</Radio>-->
-          </Radio-group>
-        </Form-item>
-        <div class="pay-tip">
-          <Icon color="#f9284f" size="16" type="alert-circled"></Icon>
-          <span class="fs-14">使用支付宝充值支付，支付宝会收取0.6%的手续费，该笔费用需要商家承担，手续费不予退还，敬请谅解！<a @click="isShowAliPayTip = true">查看支付宝官方说明</a></span>
-          <router-link v-if="memberLevel===100" class="upgroup-btn mt-10" to="/user/vip-member/order">升级VIP免除手续费</router-link>
-        </div>`
-        <Form-item>
-          <iButton class="payMoneyBtn" @click="balanceOrderCreate()">提交</iButton>
-          <iButton v-if="memberLevel===200 || memberLevel===300" class="vipRecharge ml-20" @click="showFreePayModel=true"><span v-if="memberLevel===300">S</span>VIP<span class="freeRecharge">免手续费充值</span>点击这里</iButton>
-
-          <!--<iButton class="payMoneyBtn" @click="stopRecharge = true">提交</iButton>-->
-        </Form-item>
-      </iForm>
-      <Modal v-model="payPopWindow" width="360" :styles="{top:'310px',height:'300px'}">
-        <div class="text-ct">
-          <p>请前往充值页面进行充值</p>
+    <div v-show="status === 'aliPay'" class="aliPay">
+      <div class="my-pay-desc">
+        <i-form :model="payMoney" :label-width="200" :rules="payMoneyRule" ref="payForm">
+          <form-item label="请输入充值金额:" prop="number">
+            <i-input v-model="payMoney.number" @on-blur="" style="width: 200px"/>
+            <span class="ml-10">元(最低1元起充)</span>
+          </form-item>
+          <form-item label="请选择支付方式:" prop="payMode">
+            <radio-group v-model.number="payMoney.payMode">
+              <radio label="ali">
+                <img src="~assets/img/task-release/zfb_logo.png" alt="支付宝" class="vtc-mid">
+              </radio>
+              <!--<Radio label="weiXin">-->
+              <!--<img src="~assets/img/task-release/wechat_logo.png" class="vtc-mid">-->
+              <!--</Radio>-->
+            </radio-group>
+          </form-item>
+          <div class="pay-tip">
+            <icon color="#f9284f" size="16" type="alert-circled"/>
+            <span class="fs-14">使用支付宝充值支付，支付宝会收取0.6%的手续费，该笔费用需要商家承担，手续费不予退还，敬请谅解！<a @click="isShowAliPayTip = true">查看支付宝官方说明</a></span>
+            <router-link v-if="memberLevel===100" class="upgroup-btn mt-10" to="/user/vip-member/order">升级VIP免除手续费</router-link>
+          </div>
+          <form-item>
+            <i-button class="payMoneyBtn" @click="balanceOrderCreate()">提交</i-button>
+            <i-button v-if="memberLevel===200 || memberLevel===300" class="vipRecharge ml-20" @click="toArtificialPay"><span v-if="memberLevel===300">S</span>VIP<span class="freeRecharge">免手续费充值</span>点击这里</i-button>
+            <!--<i-button class="payMoneyBtn" @click="stopRecharge = true">提交</i-button>-->
+          </form-item>
+        </i-form>
+      </div>
+      <div class="common-question">
+        <h2>常见问题</h2>
+        <div class="mt-10">
+          <h3>1、充值多久后到账？</h3>
+          <p>答：充值成功后，如果账户显示的余额不变，请您不要惊慌，我们的系统是有缓冲时间的，您只需要耐心稍等即可。</p>
         </div>
-        <div slot="footer">
-          <iButton type="success" style="width: 150px;" @click="success">已完成充值</iButton>
-          <iButton type="error" style="width: 150px;" @click="error">充值遇到问题</iButton>
-        </div>
-      </Modal>
-      <Modal v-model="payPopWindowWX"
-             :styles="{top:'310px'}">
-        <div slot="header" class="text-ct">微信支付二维码</div>
-        <div class="text-ct">
-          <img :src="imgSrc" alt="">
-        </div>
-        <div slot="footer" class="text-ct">
-          <iButton type="success" style="width: 120px;padding: 10px 10px;background-color: #FF6600;border: none"
-                   @click="success">充值成功
-          </iButton>
-          <iButton type="error"
-                   style="width: 120px;padding: 10px 10px;margin-left: 50px;background-color: #3FC0C5;border: none"
-                   @click="error">充值失败
-          </iButton>
-        </div>
-      </Modal>
-      <modal v-model="isShowAliPayTip">
-        <img src="~assets/img/common/ali-pay-tip.jpg">
-      </modal>
+      </div>
     </div>
-    <!--<Modal v-model="stopRecharge" width="360">
-      <p slot="header" style="color:#f9284f;text-align:center">
-        <Icon type="md-alert"></Icon>
-        <span>系统维护</span>
+    <div v-show="status === 'artificialPay'" class="artificialPay fs-14 cl000">
+      <p class="pt-10 pb-10 pl-5">
+        <icon type="md-information-circle" color="#f9284f" size="16" class="vtc-text-btm"/>
+        7*24小时 自动到账
       </p>
-      <div style="text-align:center">
-        <p style="height: 10px;"></p>
-        <p class="fs-14">支付宝充值系统正在维护中，请稍后再试！</p>
-        <p style="height: 10px;"></p>
+      <div class="point-box">
+        <p>银行卡注意事项：</p>
+        <p>1，人工审核对账，免手续费</p>
+        <p>2，建议使用手机银行APP转账，绝大多数手机银行APP都免手续费</p>
+        <p>3，由于银行间转账存在一定的时差，建议您使用实时到账，联系客服确认到账</p>
+        <p>4，当天充值金额请勿重复！否则只会到账一笔！</p>
+      </div>
+      <div class="step-box mt-10">
+        <h2>第一步：转账到试客多官方账号：</h2>
+        <div class="step-one pt-20 pb-20 pl-15">
+          <p>到账时间：<span class="main-color">自动到账，7*24小时</span></p>
+          <p>民生银行 <span class="main-color">6226 2277 0838 9139</span> （户名：郭天亮 开户行：民生银行杭州城东支行）</p>
+        </div>
+        <h2>第二步：每次打完钱都必需提交如下信息，否则<span>无法到账</span></h2>
+        <div class="step-two">
+          <i-form ref="form" :model="rechargeApplyInfo" :rules="applyRule">
+            <form-item label="转账金额" prop="rechargeFee">
+              <i-input class="width-150 mr-10" v-model.number="rechargeApplyInfo.rechargeFee"></i-input>
+              <span class="mr-10">元</span>
+              <span class="main-color fs-16">当天充值金额请勿重复！</span>
+            </form-item>
+            <form-item label="支付渠道" prop="payChannel">
+              <radio-group v-model="rechargeApplyInfo.payChannel" @on-change="channelChange">
+                <radio label="ebank_pc">PC版网上银行</radio>
+                <radio label="ebank_app">手机银行APP</radio>
+                <radio label="alipay">支付宝转账到银行卡</radio>
+              </radio-group>
+            </form-item>
+            <div>
+              <form-item :label="rechargeApplyInfo.payChannel === 'alipay' ? '支付宝绑定姓名': '转出银行卡持卡人姓名'" prop="name">
+                <i-input v-model="rechargeApplyInfo.name" class="width-150"></i-input>
+              </form-item>
+              <form-item :label="rechargeApplyInfo.payChannel === 'alipay' ? '转出渠道' : '转出银行名称'" prop="outBank">
+                <i-select v-model="rechargeApplyInfo.outBank" class="width-pct-39">
+                  <i-option value="支付宝" label="支付宝">支付宝</i-option>
+                  <i-option v-for="(bank,index) in bankList" :key="index" :value="bank" :label="bank">{{bank}}</i-option>
+                </i-select>
+              </form-item>
+            </div>
+            <form-item label="转入银行名称" prop="inBank">
+              <i-select v-model="rechargeApplyInfo.inBank" placeholder="请选择您转入的银行名称..." class="width-pct-39">
+                <i-option :value="item.label" :label="item.label" v-for="(item,index) in inBankList" :key="index">{{item.label}}</i-option>
+              </i-select>
+            </form-item>
+          </i-form>
+          <p class="main-color lht28 fs-18 f-b">当天充值金额请勿重复，每次充不同金额到银行（如充值5001元,5002元等做区分），转账金额不要重复！（目前平台已实现自动到账，确保功能稳定，请勿提交重复金额订单，谢谢支持）</p>
+          <p class="main-color fs-16">注：请勿使用企业支付宝或者公司网银转账，否则不会自动到账！</p>
+          <div class="mt-20">
+            <i-button class="artificialPaySubmit" :loading="rechargeApplyLoading" @click="applySubmit" :disabled="disabled" :class="{'disabled-btn':disabled}">提交</i-button>
+            <span v-if="showTimeDown" class="submit-limit">请于<time-down :endTime="limitTime" stage="artificialPay" @timeEnd="timeEnd"></time-down>后再提交</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Modal v-model="payPopWindow" width="360" :styles="{top:'310px',height:'300px'}">
+      <div class="text-ct">
+        <p>请前往充值页面进行充值</p>
       </div>
       <div slot="footer">
-        <iButton type="error" size="large" long @click="stopRecharge = false" >确定</iButton>
+        <i-button type="success" style="width: 150px;" @click="success">已完成充值</i-button>
+        <i-button type="error" style="width: 150px;" @click="error">充值遇到问题</i-button>
       </div>
-    </Modal>-->
-    <div class="common-question">
-      <h2>常见问题</h2>
-      <div class="mt-10">
-        <h3>1、充值多久后到账？</h3>
-        <p>答：充值成功后，如果账户显示的余额不变，请您不要惊慌，我们的系统是有缓冲时间的，您只需要耐心稍等即可。</p>
+    </Modal>
+    <Modal v-model="payPopWindowWX"
+           :styles="{top:'310px'}">
+      <div slot="header" class="text-ct">微信支付二维码</div>
+      <div class="text-ct">
+        <img :src="imgSrc" alt="">
       </div>
-    </div>
-    <!--<div v-if="showFreePayModel">-->
-      <!--<ArtificialRechargeModel></ArtificialRechargeModel>-->
-    <!--</div>-->
+      <div slot="footer" class="text-ct">
+        <i-button type="success" style="width: 120px;padding: 10px 10px;background-color: #FF6600;border: none"
+                 @click="success">充值成功
+        </i-button>
+        <i-button type="error"
+                 style="width: 120px;padding: 10px 10px;margin-left: 50px;background-color: #3FC0C5;border: none"
+                 @click="error">充值失败
+        </i-button>
+      </div>
+    </Modal>
+    <modal v-model="isShowAliPayTip">
+      <img src="~assets/img/common/ali-pay-tip.jpg">
+    </modal>
+    <!--人工充值申请成功提交后的提示弹窗-->
+    <modal v-model="isSuccessApply" title="提示">
+      <p class="fs-16 f-b cl000">已成功提交，若15分钟后没到账请联系客服处理！</p>
+    </modal>
     <artificial-recharge-model v-if="showFreePayModel" @colseFreePayModal="showFreePayModel=false"></artificial-recharge-model>
+    <!--<Modal v-model="stopRecharge" width="360">
+    <p slot="header" style="color:#f9284f;text-align:center">
+      <Icon type="information-circled"></Icon>
+      <span>系统维护</span>
+    </p>
+    <div style="text-align:center">
+      <p style="height: 10px;"></p>
+      <p class="fs-14">支付宝充值系统正在维护中，请稍后再试！</p>
+      <p style="height: 10px;"></p>
+    </div>
+    <div slot="footer">
+      <i-button type="error" size="large" long @click="stopRecharge = false" >确定</i-button>
+    </div>
+  </Modal>-->
   </div>
 </template>
 <script>
-  import {Icon, Form, Input, Radio, Button, Modal} from 'iview'
+  import {Icon, Form, Input, Radio, Button, Modal, Select, Option} from 'iview'
   import api from '@/config/apiConfig'
-  import {isNumber} from '@/config/utils'
+  import {isNumber, getSeverTime, setStorage, getStorage, removeStorage} from '@/config/utils'
   import {aliPayUrl, weiXinPayUrl} from '@/config/env'
   import ArtificialRechargeModel from '@/components/ArtificialRechargeModel';
+  import TimeDown from '@/components/TimeDown'
+  import commonConfig from '@/config/commonConfig'
 
   export default {
     name: 'PayMoney',
@@ -109,7 +172,10 @@
       ButtonGroup: Button.Group,
       Icon: Icon,
       Modal: Modal,
-      ArtificialRechargeModel:ArtificialRechargeModel
+      iSelect:Select,
+      iOption:Option,
+      ArtificialRechargeModel:ArtificialRechargeModel,
+      TimeDown: TimeDown
     },
     data() {
       const validatePayNumber = (rule, value, callback) => {
@@ -121,6 +187,45 @@
           callback(new Error('最低一元起充,请您重新输入'))
         } else {
           callback()
+        }
+      };
+      const validateRechargeFee = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('请输入充值金额'));
+        } else if (value * 100 === this.seasonVipFee || value * 100 === this.halfYearVipFee || value * 100 === this.yearVipFee) {
+          callback(new Error('转账金额请勿与VIP价格（1199、1999、2999）相同！'));
+        } else if (!(/^[0-9]+(.[0-9]{1,2})?$/.test(this.rechargeApplyInfo.rechargeFee))) {
+          callback(new Error('如果金额带小数，请保留两位小数'));
+        } else {
+          callback();
+        }
+      };
+      const validateChannel = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('请输入充值渠道'));
+        } else {
+          callback();
+        }
+      };
+      const validateName = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('请输入持卡人姓名'));
+        } else {
+          callback();
+        }
+      };
+      const validateOutBank = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('请输入转出银行名称'));
+        } else {
+          callback();
+        }
+      };
+      const validateInBank = (rule, value, callback) => {
+        if (!value) {
+          callback(new Error('请选择转入银行名称'));
+        } else {
+          callback();
         }
       };
       return {
@@ -138,12 +243,62 @@
         payPopWindow: false,
         payPopWindowWX: false,
         isShowAliPayTip: false,
-        showFreePayModel:false
+        showFreePayModel:false,
+        status: 'aliPay',
+        rechargeApplyInfo: {
+          rechargeFee: null,
+          payChannel: null,
+          name: null,
+          outBank: null,
+          inBank: null,
+        },
+        rechargeApplyLoading: false,
+        inBankList:[
+          {
+            label: '浙商银行'
+          },
+        ],
+        bankList: commonConfig.bankList,
+        applyRule: {
+          rechargeFee: [
+            {validator: validateRechargeFee, trigger: 'blur'}
+          ],
+          payChannel: [
+            {validator: validateChannel, trigger: 'change'}
+          ],
+          name: [
+            {validator: validateName, trigger: 'blur'}
+          ],
+          outBank: [
+            {validator: validateOutBank, trigger: 'blur'}
+          ],
+          inBank: [
+            {validator: validateInBank, trigger: 'change'}
+          ]
+        },
+        seasonVipFee: null,
+        halfYearVipFee: null,
+        yearVipFee: null,
+        isSuccessApply: false,
+        showTimeDown: false,
+        limitTime: null,
+        disabled: false
       }
     },
     mounted() {
+      if (this.status === 'aliPay') {
+        this.$refs.form.resetFields();
+      } else if (this.status === 'artificialPay') {
+        this.$refs.payForm.resetFields();
+      }
     },
     created() {
+      this.getMemberVersionPeriodList();
+      if (getStorage('limitTime')) {
+        this.limitTime = getStorage('limitTime');
+        this.showTimeDown = true;
+        this.disabled = true;
+      }
     },
     computed: {
       getUserBalance: function () {
@@ -211,6 +366,94 @@
           });
         }
       },
+      // 获取所有会员版本周期列表
+      getMemberVersionPeriodList() {
+        const _this = this;
+        api.getMemberVersionPeriodList().then(res => {
+          if (res.status) {
+            let tempData = res.data;
+            let seasonVip = tempData.find(item => {
+              return item.level === 200 && item.validDays === 91;
+            });
+            let halfYearVip = tempData.find(item => {
+              return item.level === 200 && item.validDays === 182;
+            });
+            let yearVip = tempData.find(item => {
+              return item.level === 200 && item.validDays === 365;
+            });
+            _this.seasonVipFee = seasonVip.fee;
+            _this.halfYearVipFee = halfYearVip.fee;
+            _this.yearVipFee = yearVip.fee;
+          } else {
+            _this.$Message.error(res.msg);
+          }
+        })
+      },
+      channelChange(value) {
+        this.rechargeApplyInfo.name = null;
+        this.rechargeApplyInfo.outBank = null;
+      },
+      timeEnd() {
+        this.showTimeDown = false;
+        this.disabled = false;
+        removeStorage('limitTime');
+      },
+      // 人工充值申请提交
+      applySubmit() {
+        const _this = this;
+        if (!_this.rechargeApplyInfo.rechargeFee) {
+          _this.$Message.info('请输入充值金额');
+          return
+        }
+        if (!(/^[0-9]+(.[0-9]{1,2})?$/.test(_this.rechargeApplyInfo.rechargeFee))) {
+          _this.$Message.info('请保留两位小数');
+          return
+        }
+        if (!_this.rechargeApplyInfo.payChannel) {
+          _this.$Message.info('请选择充值渠道');
+          return
+        }
+        if (!_this.rechargeApplyInfo.name) {
+          _this.$Message.info('请输入持卡人姓名');
+          return
+        }
+        if (!_this.rechargeApplyInfo.outBank) {
+          _this.$Message.info('请输入转出银行名称');
+          return
+        }
+        if (!_this.rechargeApplyInfo.inBank) {
+          _this.$Message.info('请选择转入银行名称');
+          return
+        }
+        _this.rechargeApplyLoading = true;
+        api.rechargeApply({
+          money: Math.ceil(_this.rechargeApplyInfo.rechargeFee * 100),
+          channel: _this.rechargeApplyInfo.payChannel,
+          payName: _this.rechargeApplyInfo.name,
+          bankFrom: _this.rechargeApplyInfo.outBank,
+          bankIn: _this.rechargeApplyInfo.inBank,
+          payDay: getSeverTime()
+        }).then(res => {
+          if (res.status) {
+            _this.$Message.success('已成功提交申请');
+            _this.$store.dispatch('getUserInformation');
+            _this.$refs.payForm.resetFields();
+            _this.limitTime = getSeverTime() + 5 * 60 * 1000;
+            setStorage('limitTime',_this.limitTime);
+            _this.disabled = true;
+            _this.showTimeDown = true;
+            for(let key in _this.rechargeApplyInfo) {
+              _this.rechargeApplyInfo[key] = null;
+            }
+          } else {
+            _this.$Message.error(res.msg);
+          }
+          _this.rechargeApplyLoading = false;
+        })
+      },
+      toArtificialPay() {
+        this.status = 'artificialPay';
+      }
     }
   }
 </script>
@@ -231,7 +474,40 @@
     .freeRecharge{
       color: #f7fc08;
     }
-
+  }
+  .artificialPay {
+    .point-box {
+      background: #FFF5E2;
+      border-radius: 3px;
+      line-height: 30px;
+      padding: 15px 10px;
+    }
+    .step-box {
+      border: 1px solid #f8f8f8;
+      h2 {
+        background: #F8F8F8;
+        padding: 15px;
+      }
+      .step-two {
+        padding: 20px 0 40px 40px;
+      }
+      .artificialPaySubmit {
+        background: $mainColor;
+        color: #fff;
+        width: 200px;
+        margin-left: 10%;
+      }
+      .disabled-btn {
+        background: #ccc;
+      }
+    }
+    .submit-limit {
+      padding: 5px 15px;
+      border:1px solid #ccc;
+      border-radius: 5px;
+      margin-left: 20px;
+      transform: translateY(20px);
+    }
   }
 </style>
 
