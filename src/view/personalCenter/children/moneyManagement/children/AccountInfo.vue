67db3b400a706754a2894fd5030142737ead61d5
ycb <yangzhengming@zhibi365.com>
2018-09-23 18:56:46 +0800

    代码优化

--- src/view/personalCenter/children/moneyManagement/children/AccountInfo.vue
+++ src/view/personalCenter/children/moneyManagement/children/AccountInfo.vue
@@ -36,7 +36,7 @@
           </div>
           <div>绑定手机：{{userList.phone}}</div>
           <div>注册时间：{{userList.createTime | dateFormat('YYYY-MM-DD hh:mm:ss')}}</div>
-          <p v-if="Object.keys(userList.extension).length > 0">上次登录时间：{{userList.extension.lastLoginTimePc | dateFormat('YYYY-MM-DD hh:mm:ss')}}</p>
+          <p v-if="Object.keys(userList).length > 0">上次登录时间：{{userList.extension.lastLoginTimePc | dateFormat('YYYY-MM-DD hh:mm:ss')}}</p>
         </div>
     </div>
     <div class="trading-record ">
